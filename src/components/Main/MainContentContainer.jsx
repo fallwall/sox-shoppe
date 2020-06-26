f446c8ccec01fbc38b5f2f916c5e17f8a3fb94eb
XX <fallwall19@gmail.com>
2020-06-25 18:42:04 -0700

    main home content+import data from json

--- src/components/Main/MainContentContainer.jsx
+++ src/components/Main/MainContentContainer.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import './MainContentContainer.css';
 
 const MainContentContainer = (props) => {
+
+  const style = {
+    backgroundImage: 'url(' + props.info.img + ')'
+  };
+
   return (
     <div className="main-content-container">
-      <div className="main-content-container-content">
-        <div className="main-content-container-fixed">This is a title</div>
+      <div className="main-content-container-content" style={style}>
+        <div className="main-content-container-fixed">{props.info.title}</div>
         <div className="main-content-container-unfixed">
-          <h1>BIG TITLE</h1>
+          <h1>{props.info.title}</h1>
           <div>Shop the Collection</div>
         </div>
       </div>
--- src/pages/MainHome.jsx
+++ src/pages/MainHome.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
 import MainContentContainer from '../components/Main/MainContentContainer';
+import data from '../data/mainHome.json';
 
 const MainHome = () => {
   return (
     <div className="main-home">
-      <MainContentContainer />
-      <MainContentContainer />
-      <MainContentContainer />
-      <MainContentContainer />
-      <MainContentContainer />
-      <MainContentContainer />
-      <MainContentContainer />
-      <MainContentContainer />
-      <MainContentContainer />
-      <MainContentContainer />
-      <MainContentContainer />
-      <MainContentContainer />
-      <MainContentContainer />
+      {data.data.map(e => <MainContentContainer info={e}/>)}
     </div>
   )
 }
