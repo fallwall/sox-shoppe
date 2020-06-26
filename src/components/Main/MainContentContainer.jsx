06ce2eeb76c50800c3c71a8f41f2d5c17c4c8fda
XX <fallwall19@gmail.com>
2020-06-25 18:06:25 -0700

    main home content+background img and title format

--- src/components/Main/MainContentContainer.jsx
+++ src/components/Main/MainContentContainer.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import './MainContentContainer.css';
 
-const MainContentContainer = () => {
+const MainContentContainer = (props) => {
   return (
     <div className="main-content-container">
       <div className="main-content-container-content">
         <div className="main-content-container-fixed">This is a title</div>
+        <div className="main-content-container-unfixed">
+          <h1>BIG TITLE</h1>
+          <div>Shop the Collection</div>
+        </div>
       </div>
     </div>
   )
